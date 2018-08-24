5d1939e9e10cc7b630987f2535f3b089ab14ad64
Neeraj Nair <neerajnair3@gmail.com>
2018-08-24 21:20:10 +0530

    Version 1.3

---
+++ self-describing-number-prgm.py
@@ -0,0 +1,15 @@
+# Self Describing Number Program
+# Prints a pretty patern of numbers that descibe themselves - https://en.wikipedia.org/wiki/Self-descriptive_number
+
+def impl(d, c, m):
+    if m < 0: return
+    if d == c[:len(d)]:
+        print(d)
+    for i in range(c[len(d)],m+1):
+        dd = d+[i]
+        if i<len(dd) and c[i]==dd[i]: continue
+        impl(dd,c[:i]+[c[i]+1]+c[i+1:],m-i)
+ 
+def self(n): impl([], [0]*(n+1), n)
+ 
+self(10)
